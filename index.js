c79d78bbb2da9b679c0163ce3198b794c86d5bea
Cauan Diniz <cauanrodiniz@gmail.com>
2024-01-20 12:56:59 -0300

    fix: notifications count were not going to zero

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const notifications = document.querySelectorAll('.notification')
 const clearAllBtn = document.getElementById('clearAll')
+const countNotifications = document.querySelector('.countNotifications')
 
 notifications.forEach(notification => {
   clearAllBtn.addEventListener('click', () => {
     notification.classList.add('read')
     notification.classList.remove('new')
+    countNotifications.textContent = '0'
   })
 })
